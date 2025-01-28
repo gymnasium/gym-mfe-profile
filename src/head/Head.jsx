85a9a24f452f94f92cc405b4e9f8207193accf2d
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-01-28 11:15:29 -0500

    feat: remove favicon references, update gym-frontend, version

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -10,7 +10,6 @@ const Head = ({ intl }) => (
     <title>
       {intl.formatMessage(messages['profile.page.title'], { siteName: getConfig().SITE_NAME })}
     </title>
-    <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
   </Helmet>
 );
 
